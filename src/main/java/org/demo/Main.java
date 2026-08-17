02964f2c6d4051dc2c933c46466471fed0afcc5d
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-08-17 11:50:27 +0530

    updating table and column name

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -9,35 +9,25 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Abhishek");
+        a1.setTech("Java");
 
-        Student s1 = new Student();
-        s1.setsName("Neha");
-        s1.setRollNo(107);
-        s1.setsAge(29);
-
-//        Student s2 = null;
-
-        // This actually loads the configuration
-        // If you want hibernate to manage your class then you have to use Annotation (@Entity)
-//        Configuration cfg = new Configuration();
-//        cfg.addAnnotatedClass(Student.class);
-//        cfg.configure();
-
-//        SessionFactory sf = cfg.buildSessionFactory();
-        SessionFactory sf = new Configuration().addAnnotatedClass(Student.class).configure().buildSessionFactory();
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(Alien.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sf.openSession();
-        s1 = session.find(Student.class, 1034);
+
         Transaction transaction = session.beginTransaction();
-//        session.persist(s1);
-//        session.merge(s1);
-        session.remove(s1);
+
+        session.persist(a1);
         transaction.commit();
 
 
-//         s2 = session.find(Student.class, 102);
-
         session.close();
         sf.close();
-        System.out.println(s1);
+
     }
 }
