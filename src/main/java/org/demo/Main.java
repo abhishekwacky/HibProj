e915a39d34a7e7a26971d99a4f691eaf7fddfd4a
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-08-17 11:17:55 +0530

    Performed CRUD operation using hibernate

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,17 +1,43 @@
 package org.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student s1 = new Student();
+        s1.setsName("Neha");
+        s1.setRollNo(107);
+        s1.setsAge(29);
+
+//        Student s2 = null;
+
+        // This actually loads the configuration
+        // If you want hibernate to manage your class then you have to use Annotation (@Entity)
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(Student.class);
+//        cfg.configure();
+
+//        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration().addAnnotatedClass(Student.class).configure().buildSessionFactory();
+        Session session = sf.openSession();
+        s1 = session.find(Student.class, 1034);
+        Transaction transaction = session.beginTransaction();
+//        session.persist(s1);
+//        session.merge(s1);
+        session.remove(s1);
+        transaction.commit();
+
+
+//         s2 = session.find(Student.class, 102);
+
+        session.close();
+        sf.close();
+        System.out.println(s1);
     }
 }
