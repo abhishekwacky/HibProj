9ce5f3a7c88043ff56bf012bd883f6b9a6800272
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-08-18 13:49:15 +0530

    fetching data using HQL

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -4,76 +4,39 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.Arrays;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Apple");
-        l1.setModel("Air M2");
-        l1.setRam(8);
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("HP");
-        l2.setModel("Probook");
-        l2.setRam(16);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Dell");
-        l3.setModel("Notebook");
-        l3.setRam(32);
-
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("Abhishek");
-        a1.setTech("Java");
-
-
-        Alien a2 = new Alien();
-        a2.setAid(102);
-        a2.setAname("Kunal");
-        a2.setTech("AI");
-
-
-        a1.setLaptops(Arrays.asList(l1, l2));
-        a2.setLaptops(Arrays.asList(l3));
-
-
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(Laptop.class)
-                .addAnnotatedClass(Alien.class)
                 .configure()
                 .buildSessionFactory();
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+        // select * from laptop where ram=16; -> SQL
+        // from laptop where ram=16; -> HQL
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
+        String brand = "Apple";
+        Query query = session.createQuery("select brand, model from Laptop where brand like ?1");
+        query.setParameter(1, brand);
+        List<Object[]> laptops = query.getResultList();
 
-        session.persist(a1);
-        session.persist(a2);
+        //Laptop l5 = session.find(Laptop.class,4);
 
-
-        transaction.commit();
-
+        for (Object[] data : laptops) {
+            System.out.println(data[0] + " " + data[1]);
+        }
 
         session.close();
-
-        Session session1 = sf.openSession();
-        Alien a5 = session1.find(Alien.class, 102);
-        //        System.out.println(a5);
-        session1.close();
         sf.close();
-
+//        System.out.println(laptops);
     }
 }
